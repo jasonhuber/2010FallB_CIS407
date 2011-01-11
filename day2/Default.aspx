dce2ad2f9872c4fe6161f5134cbe0065e9b812b1
Jason Huber <jasonhuber@gmail.com>
2011-01-10 20:09:34 -0700

    Added the simple user validation code.

---
+++ day2/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Username: <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox><br />
+        Password: <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox><br />
+        <asp:Button ID="cmdLogon" runat="server" Text="Logon" 
+            onclick="cmdLogon_Click" />    
+    </div>
+    </form>
+</body>
+</html>
---
+++ day2/Default.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void cmdLogon_Click(object sender, EventArgs e)
+    {
+        if (!invalidateUsername(txtUsername.Text))
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string sql = "select firstname, lastname from [users] where [username] = '" + username + "'";
+            sql += " and [password] = '" + password + "'";
+        }
+    }
+
+    //valid username characters:
+    //aA1_.
+
+    public bool invalidateUsername(string s)
+    {
+        bool bad = false;
+        int n = 0;
+        while (n < s.Length && !bad)
+        {
+            switch (s.Substring(n,1).ToLower())
+            {
+                case "a":
+                case "b":
+                case "c":
+                case "d": 
+                case "e":
+                case "f":
+                case "g":
+                case "h":
+                case "i":
+                case "j":
+                case "k":
+                case "l":
+                case "m":
+                case "n":
+                case "o":
+                case "p":
+                case "q":
+                case "r":
+                case "s":
+                case "t":
+                case "u":
+                case "v":
+                case "w":
+                case "x":
+                case "y":
+                case "z":
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case ".":
+                case "_":
+                    //we are good!   
+                    bad = false;
+                break;
+
+                default:
+                    bad = true;
+                    //uh oh
+                    break;
+            }
+            n++;
+        }
+        return bad;
+    }
+}
