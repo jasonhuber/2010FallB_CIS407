fc3f087749add688361fd088b05045bef6a9c54e
Jason Huber <jasonhuber@gmail.com>
2011-01-24 21:24:03 -0700

    Added select form

---
+++ day3/WebApplication1/WebApplication1/ReadData.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReadData.aspx.cs" Inherits="WebApplication1.ReadData" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Repeater ID="Repeater1" runat="server">
+            <ItemTemplate>
+                <div>
+                    firstname: <%# DataBinder.Eval(Container.DataItem, "firstname")%><br />
+                    Lastname:<%# DataBinder.Eval(Container.DataItem, "lastname")%><br />
+                    email:<%# DataBinder.Eval(Container.DataItem, "email")%><br />
+                    username:<%# DataBinder.Eval(Container.DataItem, "username")%><br />
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
+    </form>
+</body>
+</html>
---
+++ day3/WebApplication1/WebApplication1/ReadData.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class ReadData : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Data.OleDb.OleDbConnection Conn = new System.Data.OleDb.OleDbConnection();
+            Conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Server.MapPath("app_data/products.mdb");
+            Conn.Open();
+            //Response.Write(Conn.State);
+
+            System.Data.OleDb.OleDbCommand Comm = new System.Data.OleDb.OleDbCommand();
+            Comm.Connection = Conn;
+
+            Comm.CommandText = "select firstname, lastname, email, [username] from [users]";
+
+            System.Data.DataSet ds = new System.Data.DataSet();
+            System.Data.OleDb.OleDbDataAdapter da = new System.Data.OleDb.OleDbDataAdapter();
+
+            da.SelectCommand = Comm;
+            da.Fill(ds);
+
+            Repeater1.DataSource = ds.Tables[0];
+            Repeater1.DataBind();
+
+             /*  
+            //need to get a dataset or reader back here.
+            System.Data.OleDb.OleDbDataReader DR;
+            DR = Comm.ExecuteReader();
+
+            Repeater1.DataSource = DR;
+            Repeater1.DataBind();
+            */
+
+            Conn.Close();
+            Conn = null;
+        }
+    }
+}
